487c998e7c725f48f57576c48468f11e8827240a
artem <artem2006stepanov@yandex.ru>
2025-11-29 21:24:46 +0300

    peredmerge

--- backend/internal/handlers/middleware.go
+++ backend/internal/handlers/middleware.go
@@ -5,8 +5,7 @@ import (
 	"github.com/go-chi/jwtauth/v5"
 )
 
-
-func (h *Handler) authMiddleware(r chi.Router){
+func (h *Handler) authMiddleware(r chi.Router) {
 	r.Use(jwtauth.Verifier(h.services.Authorization.TokenStruct()))
 	r.Use(jwtauth.Authenticator(h.services.Authorization.TokenStruct()))
 }
