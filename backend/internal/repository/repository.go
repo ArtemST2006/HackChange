819e79c1deb837c557e70825af10129adc339a46
artem <artem2006stepanov@yandex.ru>
2025-11-29 22:01:47 +0300

    mistake

--- backend/internal/repository/repository.go
+++ backend/internal/repository/repository.go
@@ -3,35 +3,17 @@
 package repository
 
 import (
-<<<<<<< HEAD
+	"context"
+	"io"
+
+	"github.com/ArtemST2006/HackChange/internal/repository/miniorep"
 	"github.com/ArtemST2006/HackChange/internal/repository/postgres"
 
 	"github.com/ArtemST2006/HackChange/internal/schema"
 	"gorm.io/gorm"
 )
 
-// === Интерфейсы ===
 type Authorization interface {
-	//TODO methods for authorization
-}
-
-type Courses interface {
-	GetCourseDashboard(schema.DashboardRequest) (schema.DashboardResponse, error)
-	GetCourseLessons(schema.LessonsRequest) (schema.LessonsResponse, error)
-	GetCourseLesson(schema.LessonReq) (schema.LessonResp, error)
-	SignupCourse(schema.SignupCourseReq) (schema.SignupCourseResp, error)
-=======
-	"context"
-	"io"
-	"log/slog"
-
-	"github.com/ArtemST2006/HackChange/internal/repository/miniorep"
-	postgres "github.com/ArtemST2006/HackChange/internal/repository/postgres"
-	"github.com/ArtemST2006/HackChange/internal/schema"
-	"gorm.io/gorm"
-)
-
-type Authorization interface{
 	CreateUser(schema.Student) (uint, error)
 	GetUser(string) (*schema.Student, error)
 	CreateRefreshToken(schema.RefreshToken) error
@@ -40,18 +22,17 @@ type Authorization interface{
 	RevokeAllRefreshTokens(string) error
 }
 
-type Minio interface{
+type Minio interface {
 	UploadHomework(ctx context.Context, lessonID, homeworkID string, files []schema.HomeworkUploadFile) error
 	GetHomework(ctx context.Context, lessonID, homeworkID, fileName string) (io.ReadCloser, error)
 	GetHomeworkURL(ctx context.Context, lessonID, homeworkID string, fileNames []string, ttlSeconds int64) (map[string]string, error)
 }
- 
 
-
-type Repository struct{
-	Authorization 
-	Minio
->>>>>>> origin/auth
+type Courses interface {
+	GetCourseDashboard(schema.DashboardRequest) (schema.DashboardResponse, error)
+	GetCourseLessons(schema.LessonsRequest) (schema.LessonsResponse, error)
+	GetCourseLesson(schema.LessonReq) (schema.LessonResp, error)
+	SignupCourse(schema.SignupCourseReq) (schema.SignupCourseResp, error)
 }
 
 type User interface {
@@ -61,7 +42,6 @@ type User interface {
 	UserChangePass(userID uint, passwords *schema.UserChangePassReq) error
 }
 
-<<<<<<< HEAD
 type Comment interface {
 	CreateCourseComment(comment *schema.CommentCourse) error
 	GetCourseCommentsByCourseID(courseID uint) ([]schema.CommentCourseWithUser, error)
@@ -73,6 +53,7 @@ type Comment interface {
 
 type Repository struct {
 	Authorization
+	Minio
 	User
 	Courses
 	Comment
@@ -83,13 +64,8 @@ func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{
 		Authorization: nil,
 		Courses:       postgres.NewCoursesRepository(db),
-		User:          NewUserRepo(db),
-		Comment:       NewCommentPostgres(db),
-=======
-func NewRepository(db *gorm.DB,log *slog.Logger) *Repository{
-	return &Repository{
-		Authorization: postgres.NewAuthPostgres(db),
-		Minio: miniorep.NewMinioClient(),
->>>>>>> origin/auth
+		User:          postgres.NewUserRepo(db),
+		Comment:       postgres.NewCommentPostgres(db),
+		Minio:         miniorep.NewMinioClient(),
 	}
 }
